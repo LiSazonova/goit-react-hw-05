6a4b09e82d856bec7677bec5a1e10b576c04486e
Lili Sazonova <lilissazonova@gmail.com>
2024-07-24 16:02:10 +0300

    add styles NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,13 +1,15 @@
 import { Suspense } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import s from './NotFoundPage.module.css';
 
 const NotFoundPage = () => {
   return (
     <main>
-      <h3>Not Found</h3>
-      <p>
-        <Link to="/">Go Home</Link>
-      </p>
+      <Link to="/" className={s.linkHome}>
+        Go Home
+      </Link>
+      <h3 className={s.title}>Page Not Found</h3>
+
       <Suspense fallback={<div>Loading subpage...</div>}>
         <Outlet />
       </Suspense>
